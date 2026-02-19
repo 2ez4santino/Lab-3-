2406f899d5380da441dffffd8ca60f1c03e7ca3f
A202-PC24 <masragadio@tip.edu.ph>
2026-02-19 12:06:32 +0800

    feat: add contact details and academic history sections

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="container">
@@ -6,24 +8,125 @@ function App() {
       <form>
         {/* Personal Information */}
         <fieldset>
-          <legend>Personal Information</legend>
-        </fieldset>
-
-        {/* Contact Details */}
-        <fieldset>
           <legend>Contact Details</legend>
-        </fieldset>
 
-        {/* Academic History */}
         <fieldset>
-          <legend>Academic History</legend>
+  <legend>Academic History – Grade School</legend>
+
+  <fieldset>
+  <legend>Enrollment Choices</legend>
+
+  <h3>Academic Level</h3>
+  <label><input type="radio" name="level" /> Undergraduate</label>
+  <label><input type="radio" name="level" /> Graduate</label>
+
+  <h3>Semester</h3>
+  <label><input type="radio" name="semester" /> 1st Semester</label>
+  <label><input type="radio" name="semester" /> 2nd Semester</label>
+
+  <h3>Campus</h3>
+  <label><input type="radio" name="campus" /> Manila</label>
+  <label><input type="radio" name="campus" /> Quezon City</label>
+</fieldset>
+
+
+  <div className="grid-2">
+    <div>
+      <label>School Name</label>
+      <input type="text" />
+    </div>
+
+    <div>
+      <label>Year Graduated</label>
+      <input type="number" min="1900" max="2026" />
+    </div>
+  </div>
+
+  <div>
+    <label>School Address</label>
+    <input type="text" />
+  </div>
+</fieldset>
+
+
+  <div className="grid-2">
+    <div>
+      <label>Email Address</label>
+      <input type="email" required />
+    </div>
+
+    <div>
+      <label>Mobile Number</label>
+      <input type="tel" required />
+    </div>
+  </div>
+
+  <div className="grid-3">
+    <div>
+      <label>Street</label>
+      <input type="text" />
+    </div>
+
+    <div>
+      <label>City</label>
+      <input type="text" />
+    </div>
+
+    <div>
+      <label>Zip Code</label>
+      <input type="text" />
+    </div>
+  </div>
+
+          
+          <legend>Personal Information</legend>
+
+          {/* Name Grid */}
+          <div className="grid-4">
+            <div>
+              <label>First Name</label>
+              <input type="text" required />
+            </div>
+
+            <div>
+              <label>Middle Name</label>
+              <input type="text" />
+            </div>
+
+            <div>
+              <label>Last Name</label>
+              <input type="text" required />
+            </div>
+
+            <div>
+              <label>Suffix</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="grid-2">
+            <div>
+              <label>Date of Birth</label>
+              <input
+                type="date"
+                required
+                onKeyDown={(e) => e.preventDefault()}
+              />
+            </div>
+
+            <div>
+              <label>Gender</label>
+              <select required>
+                <option value="">Select</option>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Non-binary</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
 
-        {/* Enrollment Choices */}
-        <fieldset>
-          <legend>Enrollment Choices</legend>
-        </fieldset>
-
+        {/* Submit Button */}
         <button type="submit">Submit Registration</button>
       </form>
     </div>
